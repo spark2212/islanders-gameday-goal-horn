955e6b5d9ddf3702c3eca17b49bd1b24603a3d76
Schoolwork <schoolwork@Lil-McSpark.local>
2022-10-31 11:46:10 -0700

    started commenting structure

---
+++ Islanders Gameday 5.py
@@ -0,0 +1,14 @@
+
+
+try:
+    pass
+    # OPENING SEQUENCE ##################
+    # If remote Program On/Off is False, (newTrack == 20): full startup sequence
+    # Else, relaunch based on remote's state
+
+    # Threads:
+        # audio stream
+        # ringbuffer collectors
+        # hornActivationSequence
+except Exception as e:
+    pass
